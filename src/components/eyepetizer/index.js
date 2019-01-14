b270447a3a9d5ca962b65c50379a88cea01b27d1
xiaoxiang9527 <xiang.xiao@outlook.com>
2019-01-14 22:43:52 +0800

    loading2

---
+++ src/components/eyepetizer/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Eyepetizer extends Component {
+  render() {
+    return (
+      <div>
+      eye
+      </div>
+    );
+  }
+}
+
+export default Eyepetizer;
---
+++ src/stores/reducers/index.js
@@ -0,0 +1,8 @@
+import reducerEyepetizer from './reducer-eyepetizer';
+import {combineReducers} from 'redux';
+
+const rootReducer=combineReducers({
+  reducerEyepetizer
+});
+
+export default rootReducer
